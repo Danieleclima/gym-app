26fa7f3a0e5995b30e55e205398b5d917b3fa3dd
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-11-20 19:11:18 +0000

    changes

--- gym-app-frontend/index.js
+++ gym-app-frontend/index.js
@@ -17,6 +17,14 @@ function fetchGyms() {
       document.body.appendChild(mainDiv)
   }
 
+  function fetchMenu(){
+    fetch('http://localhost:3000/locations')
+  }
+
+  function createDropdown(json){
+
+  }
+
   document.addEventListener('DOMContentLoaded', (event) => {
     fetchGyms();
   })
