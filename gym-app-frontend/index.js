5fec5337cf2574e621072ae17d08ce20eaa83fd2
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-11-28 20:04:00 +0000

    gym class

--- gym-app-frontend/index.js
+++ gym-app-frontend/index.js
@@ -38,11 +38,6 @@ function fetchGyms() {
       })
   }
 
-  document.addEventListener('DOMContentLoaded', (event) => {
-    fetchMenu();
-    fetchGyms();
-  })
-
   function filterGyms() {
     fetch('http://localhost:3000/gyms')
     .then(resp => resp.json())
@@ -60,37 +55,72 @@ function filteredList(json){
     })  
 }
 
-function createGyms(gym){
-  let row = document.getElementsByClassName("row height")[0]
-  let col = document.createElement('div')
-  col.className = 'col-md-4 col-lg-6 my-3'
-    let card = document.createElement('div')
-    card.className = "card"
-    let img = document.createElement('img')
-    img.className = "card-img-top"
-    img.src = `${gym.image}`
-    let cardBody = document.createElement('div')
-    cardBody.className = "card-body"
-    let cardTitle = document.createElement('h5')
-    cardTitle.className = "card-title text-capitalize"
-    cardTitle.innerText = gym.name
-    let website = document.createElement('a')
-    website.className = "btn btn-outline-dark text-uppercase"
-    website.innerText = "Website"
-    website.href = gym.url
-    cardBody.appendChild(cardTitle)
-    cardBody.appendChild(website)
-    card.appendChild(img)
-    card.appendChild(cardBody)
-    col.appendChild(card)
-    row.appendChild(col)
+function createGyms(json){
+  let gym = new Gym(json)
+  gym.addGym()
+  // let row = document.getElementsByClassName("row height")[0]
+  // let col = document.createElement('div')
+  // col.className = 'col-md-4 col-lg-6 my-3'
+  //   let card = document.createElement('div')
+  //   card.className = "card"
+  //   let img = document.createElement('img')
+  //   img.className = "card-img-top"
+  //   img.src = `${gym.image}`
+  //   let cardBody = document.createElement('div')
+  //   cardBody.className = "card-body"
+  //   let cardTitle = document.createElement('h5')
+  //   cardTitle.className = "card-title text-capitalize"
+  //   cardTitle.innerText = gym.name
+  //   let website = document.createElement('a')
+  //   website.className = "btn btn-outline-dark text-uppercase"
+  //   website.innerText = "Website"
+  //   website.href = gym.url
+  //   cardBody.appendChild(cardTitle)
+  //   cardBody.appendChild(website)
+  //   card.appendChild(img)
+  //   card.appendChild(cardBody)
+  //   col.appendChild(card)
+  //   row.appendChild(col)
 }
 
-// function listeners(){
-//   let select = document.querySelector('select')
-//   for (let item of options) {
-//     item.addEventListener('click', (event) =>{
-//       filterGyms();
-//     })
-// }
-// }
+document.addEventListener('DOMContentLoaded', (event) => {
+  fetchMenu();
+  fetchGyms();
+})
+
+class Gym {
+  constructor({id:id , name: name, rating: rating, openinghours: openinghours, created_at: created_at, image: image, location_id: location_id, updated_at: updated_at, url: url, city_name: city_name}){
+    this.name = name;
+    this.image = image;
+    this.url = url;
+    this.city_name = city_name;
+  }
+
+  addGym(){
+    let row = document.getElementsByClassName("row height")[0]
+    let col = document.createElement('div')
+    col.className = 'col-md-4 col-lg-6 my-3'
+      let card = document.createElement('div')
+      card.className = "card"
+      let img = document.createElement('img')
+      img.className = "card-img-top"
+      img.src = `${this.image}`
+      let cardBody = document.createElement('div')
+      cardBody.className = "card-body"
+      let cardTitle = document.createElement('h5')
+      cardTitle.className = "card-title text-capitalize"
+      cardTitle.innerText = this.name
+      let website = document.createElement('a')
+      website.className = "btn btn-outline-dark text-uppercase"
+      website.innerText = "Website"
+      website.href = this.url
+      cardBody.appendChild(cardTitle)
+      cardBody.appendChild(website)
+      card.appendChild(img)
+      card.appendChild(cardBody)
+      col.appendChild(card)
+      row.appendChild(col)
+  }
+}
+
+
